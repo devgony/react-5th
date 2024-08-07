022c448e7a4f130d804b3894ad931e37db7bbb14
devgony <devgony@gmail.com>
2024-08-07 14:03:27 +0900

    D52-ZexyZod

--- D50-CarrotBasic/app/actions.ts
+++ D50-CarrotBasic/app/actions.ts
@@ -1,13 +1,30 @@
 "use server";
 
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .email()
+    .refine(
+      (email) => email.endsWith("@zod.com"),
+      "Email must end with @zod.com"
+    ),
+  username: z.string().min(5, "Username must be at least 5 characters"),
+  password: z.string().min(10).regex(/[0-9]/, "Password must contain a number"),
+});
+
 export default async function handleForm(prevState: any, formData: FormData) {
-  console.log(prevState);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const password = formData.get("password");
+  const data = {
+    email: formData.get("email"),
+    username: formData.get("username"),
+    password: formData.get("password"),
+  };
 
-  if (password !== "12345") {
-    return { errors: ["wrong password", "try 12345"] };
-  }
+  const result = formSchema.safeParse(data);
 
-  return { payload: "Login successful!" };
+  return {
+    logined: result.success,
+    ...result.error?.flatten(),
+  };
 }
--- D50-CarrotBasic/app/page.tsx
+++ D50-CarrotBasic/app/page.tsx
@@ -4,24 +4,40 @@ import { useFormState } from "react-dom";
 import FormButton from "./components/form-button";
 import handleForm from "./actions";
 import { Fragment } from "react";
+import Input from "./components/input";
 
 export default function Home() {
-  const [state, action] = useFormState(handleForm, null);
-  if (state?.payload) {
-    alert(state.payload);
-  }
+  const [state, dispatch] = useFormState(handleForm, null);
   return (
-    <main className="flex justify-center items-center">
-      <form action={action} className="flex flex-col">
-        <input name="email" type="email" placeholder="email" />
-        <input name="username" type="text" placeholder="username" />
-        <input name="password" type="password" placeholder="password" />
-        {state?.errors?.map((e, i) => (
-          <Fragment key={i}>
-            <p className="text-red-500">{e}</p>
-          </Fragment>
-        ))}
+    <main className="flex justify-center items-center flex-col">
+      <form action={dispatch} className="flex flex-col">
+        <Input
+          name="email"
+          type="email"
+          placeholder="email"
+          required={true}
+          errors={state?.fieldErrors?.email}
+        />
+        <Input
+          name="username"
+          type="text"
+          placeholder="username"
+          required={true}
+          errors={state?.fieldErrors?.username}
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="password"
+          required={true}
+          errors={state?.fieldErrors?.password}
+        />
         <FormButton payload="Log in" />
+        {state?.logined && (
+          <output className="w-full h-10 bg-green-400 border flex justify-center items-center">
+            Welcome back!
+          </output>
+        )}
       </form>
     </main>
   );
